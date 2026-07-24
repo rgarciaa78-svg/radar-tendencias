82fc29277b076efe9b046aa674a1ee853be217ca
Renato Garcia <rgarciaa78@gmail.com>
2026-07-24 18:00:35 -0500

    fix paths

---
+++ docs/assets/__vite-browser-external-Z4zCO143.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-C9_B2paS.js";var t=e(((e,t)=>{t.exports={}}));export default t();
--- docs/assets/__vite-browser-external-cpb1W-8K.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-Cr6gAaj4.js";var t=e(((e,t)=>{t.exports={}}));export default t();
