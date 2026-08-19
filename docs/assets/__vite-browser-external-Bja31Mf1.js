59f5733d361a5019a798f37c3cb1894c0a3692ed
Renato Garcia <rgarciaa78@gmail.com>
2026-08-18 22:07:37 -0500

    Mejora DETECTADA: 5 trends débiles → Descartada + 4 oportunidades reales confirmadas (conservas gourmet, frutas liofilizadas, pasta de quinua, bone broth)

---
+++ docs/assets/__vite-browser-external-Bja31Mf1.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-DQ5MxlXP.js";var t=e(((e,t)=>{t.exports={}}));export default t();
--- docs/assets/__vite-browser-external-zSa8LKGA.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-oRGmfK4Z.js";var t=e(((e,t)=>{t.exports={}}));export default t();
