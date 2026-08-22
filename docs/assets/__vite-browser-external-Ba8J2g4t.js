300ca86d510d03d4b8a837a26795779a277518e1
Renato Garcia <rgarciaa78@gmail.com>
2026-08-21 22:07:21 -0500

    fix: stack proyectos header vertically on mobile to prevent overlap

---
+++ docs/assets/__vite-browser-external-Ba8J2g4t.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-Cuob5LM0.js";var t=e(((e,t)=>{t.exports={}}));export default t();
--- docs/assets/__vite-browser-external-D61CnH3Y.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-DV4dNQ3x.js";var t=e(((e,t)=>{t.exports={}}));export default t();
